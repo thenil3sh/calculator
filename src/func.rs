4d2998d8c12024809c2ad3e8de2a6acf4f8e6a8f
oreo3494 <nileshkumar.newdelhi@gmail.com>
2024-10-25 23:04:59 +0530

    now adapts theme (at launch only)

---
+++ src/func.rs
@@ -0,0 +1,82 @@
+// fn sum<T: Into<f64>, U: Into<f64>>(x: T, y: U) -> f64 {
+//     x.into() + y.into()
+// }
+
+// fn multiply<T: Into<f64>, U: Into<f64>>(x: T, y: U) -> f64 {
+//     x.into() + y.into()
+// }
+
+// fn divide<T: Into<f64>, U: Into<f64>>(x: T, y: U) -> f64 {
+//     x.into() / y.into()
+// }
+
+// fn subtract<T: Into<f64>, U: Into<f64>>(x: T, y: U) -> f64 {
+//     x.into() - y.into()
+// }
+slint::include_modules!();
+#[derive(Debug)]
+enum Operator {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+
+#[derive(Debug)]
+
+enum Paren {
+    Left,
+    Right,
+}
+
+#[derive(Debug)]
+enum Expression<T: Into<f64>> {
+    Number(T),
+    Parenthesis(Paren),
+    Op(Operator),
+}
+
+use Expression::{Number, Op, Parenthesis};
+use Operator::{Add, Divide, Multiply, Subtract};
+use Paren::{Left, Right};
+
+fn main() {
+    let app = AppWindow::new().unwrap();
+    app.run().unwrap();
+    let expression_string = "34 + (45/34)/2 ";
+    let mut expression: Vec<Expression<f64>> = Vec::new();
+
+    let mut num = String::new();
+    let mut last_i: char = ' ';
+
+    for i in expression_string.chars() {
+        //let peri = i != ' ' && i != '\'';
+        if i.is_numeric() {
+            num.push(i);
+        } else if last_i.is_numeric() && !num.is_empty(){
+            println!("Pushing {num}!!");
+            expression.push(Number(num.parse().unwrap()));
+            num.clear();
+        }
+        println!("{:?}", expression);
+        if i != ' ' {
+            last_i = i
+        } else {
+            continue;
+        };
+        if !i.is_numeric() {
+            expression.push(match i {
+                '(' => Parenthesis(Left),
+                ')' => Parenthesis(Right),
+                '+' => Op(Add),
+                '-' => Op(Subtract),
+                '*' => Op(Multiply),
+                '/' => Op(Divide),
+                ' ' => continue,
+                '.' => continue,
+                _ => panic!("Bro wtf"),
+            });
+        }
+    }
+    println!("Finally : {:?}", expression);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,34 @@
-// fn sum<T: Into<f64>, U: Into<f64>>(x: T, y: U) -> f64 {
-//     x.into() + y.into()
-// }
-
-// fn multiply<T: Into<f64>, U: Into<f64>>(x: T, y: U) -> f64 {
-//     x.into() + y.into()
-// }
-
-// fn divide<T: Into<f64>, U: Into<f64>>(x: T, y: U) -> f64 {
-//     x.into() / y.into()
-// }
-
-// fn subtract<T: Into<f64>, U: Into<f64>>(x: T, y: U) -> f64 {
-//     x.into() - y.into()
-// }
+use slint::{self, SharedString};
+use std::cell::RefCell;
+use std::rc::Rc;
 slint::include_modules!();
-#[derive(Debug)]
-enum Operator {
-    Add,
-    Subtract,
-    Multiply,
-    Divide,
-}
-
-#[derive(Debug)]
-
-enum Paren {
-    Left,
-    Right,
-}
-
-#[derive(Debug)]
-enum Expression<T: Into<f64>> {
-    Number(T),
-    Parenthesis(Paren),
-    Op(Operator),
-}
-
-use Expression::{Number, Op, Parenthesis};
-use Operator::{Add, Divide, Multiply, Subtract};
-use Paren::{Left, Right};
+use dark_light::Mode;
 
 fn main() {
-    let app = AppWindow::new().unwrap();
-    app.run().unwrap();
-    let expression_string = "34 + (45/34)/2 ";
-    let mut expression: Vec<Expression<f64>> = Vec::new();
+    let window = AppWindow::new().unwrap();
+    let oreo = Rc::new(RefCell::new(String::new()));
+    window.global::<theme>().set_dark(    match dark_light::detect() {
+        Mode::Dark => true,
+        Mode::Light => false,
+        _ => true,
+    });
 
-    let mut num = String::new();
-    let mut last_i: char = ' ';
 
-    for i in expression_string.chars() {
-        //let peri = i != ' ' && i != '\'';
-        if i.is_numeric() {
-            num.push(i);
-        } else if last_i.is_numeric() && !num.is_empty(){
-            println!("Pushing {num}!!");
-            expression.push(Number(num.parse().unwrap()));
-            num.clear();
-        }
-        println!("{:?}", expression);
-        if i != ' ' {
-            last_i = i
-        } else {
-            continue;
-        };
-        if !i.is_numeric() {
-            expression.push(match i {
-                '(' => Parenthesis(Left),
-                ')' => Parenthesis(Right),
-                '+' => Op(Add),
-                '-' => Op(Subtract),
-                '*' => Op(Multiply),
-                '/' => Op(Divide),
-                ' ' => continue,
-                '.' => continue,
-                _ => panic!("Bro wtf"),
-            });
-        }
-    }
-    println!("Finally : {:?}", expression);
+    
+    
+    let weak = window.as_weak();
+    let pika = Rc::clone(&oreo);
+    window.global::<elements>().on_touch(move |str|{
+        let app = weak.upgrade().unwrap();
+        let mut oreo = pika.borrow_mut();
+        oreo.push(str.to_string().chars().nth(0).unwrap());
+        app.global::<elements>().set_text(SharedString::from(&*oreo));
+    });
+    
+    
+    
+    
+    
+    window.run().unwrap();
+    
 }
